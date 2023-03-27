247a736a77864c09ef4e093e1fe6c86f31ab1526
waltersimekha <waltersimekha@gmail.com>
2023-03-27 12:48:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// const newHeader = document.createElement('h1');
+// newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
